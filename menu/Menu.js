07d25109bcfc55d1d313708959a01f0d00bbbbbe
Han Trinh Viet <hntrnn12@gmail.com>
2022-10-04 22:59:22 +0700

    aboutUs Commit

---
+++ menu/Menu.js
@@ -0,0 +1,11 @@
+console.log("Active")
+
+
+function arrowOver(){
+    document.getElementById("order").innerHTML = "<i class='fas fa-angle-double-right'></i>"
+}
+
+function arrowOut(){
+    document.getElementById("order").innerHTML = "ORDER NOW"
+}
+
